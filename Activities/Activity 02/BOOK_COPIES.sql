9844fd89c3a04263cde544c679a581551fb745a0
Marhabo <91888254+MarhaboR@users.noreply.github.com>
2022-02-21 20:00:29 +0600

    Create BOOK_COPIES.sql

---
+++ Activities/Activity 02/BOOK_COPIES.sql
@@ -0,0 +1,8 @@
+-- create BOOK_COPIES (Book_id, Branch_id, No-of_Copies) 
+CREATE TABLE BOOK_COPIES (
+  Book_id INT (20),
+  Branch_id  CHAR(20),
+  No-of_Copies INT(20),
+  PRIMARY KEY (Book_id)
+  
+);
