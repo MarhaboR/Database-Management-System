8a20c46ccb2a65cf9b802e6fbb9d562ed4b677c0
Marhabo <91888254+MarhaboR@users.noreply.github.com>
2022-02-21 20:06:59 +0600

    Create LIBRARY_BRANCH.sql

---
+++ Activities/Activity 02/LIBRARY_BRANCH.sql
@@ -0,0 +1,8 @@
+-- create LIBRARY_BRANCH (Branch_id, Branch_Name, Address) 
+CREATE TABLE LIBRARY_BRANCH  (
+  Branch_id INT (20),
+  Branch_Name  CHAR(20),
+  Address CHAR(20),
+  PRIMARY KEY (Branch_id)
+  
+);
