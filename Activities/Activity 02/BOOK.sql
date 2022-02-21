7bcde70b0ba15dc868799adb24fabf99308d32b6
Marhabo <91888254+MarhaboR@users.noreply.github.com>
2022-02-21 20:08:32 +0600

    Create BOOK.sql

---
+++ Activities/Activity 02/BOOK.sql
@@ -0,0 +1,9 @@
+-- creating Book table (Book_id, Title, Publisher_Name, Pub_Year) with Book_id being a primary key)
+CREATE TABLE BOOK (
+  Book-id INT (20),
+  Title CHAR(20),
+  Publisher_Name CHAR(50),
+  Publisher_Year INT(20),
+  PRIMARY KEY (Book-id)
+  
+);
